3bb9538788ae950c86164bced8e970447f70da5c
Ianthe23 <ivo.pastin@gmail.com>
2024-12-11 18:49:28 +0200

    added type to FeedbackItem

--- corp-comment/src/components/FeedbackList.tsx
+++ corp-comment/src/components/FeedbackList.tsx
@@ -1,28 +1,29 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import FeedbackItem from "./FeedbackItem";
+
+const feedbackItems = [
+  {
+    upvoteCount: 593,
+    badgeLetter: "B",
+    companyName: "ByteGrad",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab impedit rem consectetur sapiente! Cumque, exercitationem!",
+    daysAgo: 4,
+  },
+  {
+    upvoteCount: 410,
+    badgeLetter: "C",
+    companyName: "Crunchyroll",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab impedit rem consectetur sapiente! Cumque, exercitationem!",
+    daysAgo: 3,
+  },
+];
 
 export default function FeedbackList() {
   return (
     <ol className="feedback-list">
-      <li className="feedback">
-        <button>
-          <TriangleUpIcon />
-          <span>593</span>
-        </button>
-
-        <div>
-          <p>B</p>
-        </div>
-
-        <div>
-          <p>ByteGrad</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab impedit
-            rem consectetur sapiente! Cumque, exercitationem!
-          </p>
-        </div>
-
-        <p>4d</p>
-      </li>
-    </ol>
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem feedbackItem={feedbackItem} />
+      ))}{" "}
+    </ol> // daca scriam "{}", ar fi trebuit sa returnez obiectul
+    // asa cu "()" pot sa scriu direct obiectul, nu mai trebuie sa scriu "return".
   );
 }
