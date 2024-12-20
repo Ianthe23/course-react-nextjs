c95ed579474e3167e880c66367d96e421034d18f
Ianthe23 <ivo.pastin@gmail.com>
2024-12-20 17:17:04 +0200

    added error message and spinner to item list when fetching data

--- corp-comment/src/components/FeedbackList.tsx
+++ corp-comment/src/components/FeedbackList.tsx
@@ -1,27 +1,43 @@
+import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
-
-const feedbackItems = [
-  {
-    upvoteCount: 593,
-    badgeLetter: "B",
-    companyName: "ByteGrad",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab impedit rem consectetur sapiente! Cumque, exercitationem!",
-    daysAgo: 4,
-  },
-  {
-    upvoteCount: 410,
-    badgeLetter: "C",
-    companyName: "Crunchyroll",
-    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab impedit rem consectetur sapiente! Cumque, exercitationem!",
-    daysAgo: 3,
-  },
-];
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+
+        if (!response.ok) {
+          throw new Error();
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage("An error occurred. Please try again later.");
+      }
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
   return (
+    //spinner pentru loading prima data
+    //fiecare item din lista de feedbackuri
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
+      {errorMessage && <ErrorMessage message={errorMessage} />}
       {feedbackItems.map((feedbackItem) => (
-        <FeedbackItem feedbackItem={feedbackItem} />
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}{" "}
     </ol> // daca scriam "{}", ar fi trebuit sa returnez obiectul
     // asa cu "()" pot sa scriu direct obiectul, nu mai trebuie sa scriu "return".
