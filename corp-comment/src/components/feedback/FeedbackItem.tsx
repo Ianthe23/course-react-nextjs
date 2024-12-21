941728cb3ebb1e0ff00a3dc67ea9d82cbbf3e1ed
Ianthe23 <ivo.pastin@gmail.com>
2024-12-21 11:53:05 +0200

    added functionality to Upvote button

--- corp-comment/src/components/feedback/FeedbackItem.tsx
+++ corp-comment/src/components/feedback/FeedbackItem.tsx
@@ -6,15 +6,22 @@ type FeedbackItemProps = { feedbackItem: TFeedbackItem };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount((prev) => ++prev);
+    e.currentTarget.disabled = true;
+    e.stopPropagation(); // prevents the event from bubbling up to the parent
+  };
 
   return (
     <li
       onClick={() => setOpen((prev) => !prev)}
       className={`feedback ${open ? "feedback--expand" : ""}`}
     >
-      <button>
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
 
       <div>
