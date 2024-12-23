04595c469fb955105cb9fd037f88925433d44b5f
Ianthe23 <ivo.pastin@gmail.com>
2024-12-23 10:50:26 +0200

    added loading state for the Spinner component

--- remote-dev/src/components/App.tsx
+++ remote-dev/src/components/App.tsx
@@ -16,6 +16,7 @@ import PaginationControls from "./PaginationControls";
 function App() {
   const [jobItems, setJobItems] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!searchText) {
@@ -23,10 +24,12 @@ function App() {
     }
 
     const fetchData = async () => {
+      setIsLoading(true);
       const response = await fetch(
         `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
       );
       const data = await response.json();
+      setIsLoading(false);
       setJobItems(data.jobItems);
     };
 
@@ -53,7 +56,7 @@ function App() {
             <SortingControls />
           </SidebarTop>
 
-          <JobList jobItems={jobItems} />
+          <JobList jobItems={jobItems} isLoading={isLoading} />
           <PaginationControls />
         </Sidebar>
         <JobItemContent />
