238bed80f81276a66683a4006dea26a89ad47d2a
Ianthe23 <ivo.pastin@gmail.com>
2024-12-23 11:32:28 +0200

    refactor: update useJobItems hook to return sliced job items and improve type definitions

--- remote-dev/src/lib/hooks.ts
+++ remote-dev/src/lib/hooks.ts
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import { JobItem } from "./types";
 
 export function useJobItems(searchText: string) {
-  const [jobItems, setJobItems] = useState([]);
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  // this is called derived state
+  const jobItemsSliced = jobItems.slice(0, 7); // index 7 is not included
 
   useEffect(() => {
     if (!searchText) {
@@ -22,8 +26,5 @@ export function useJobItems(searchText: string) {
     fetchData();
   }, [searchText]);
 
-  return {
-    jobItems,
-    isLoading,
-  };
+  return [jobItemsSliced, isLoading] as const;
 }
