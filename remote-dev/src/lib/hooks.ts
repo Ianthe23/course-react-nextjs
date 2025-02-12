eef1e7978ecd7ba9bb3eb9ae2e0c4c344b47fcd2
Ianthe23 <ivo.pastin@gmail.com>
2025-02-12 11:20:25 +0200

    used toast messages for errors

--- remote-dev/src/lib/hooks.ts
+++ remote-dev/src/lib/hooks.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { JobItem, JobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
 import { useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 type JobItemApiResponse = {
   public: boolean;
@@ -51,6 +52,11 @@ const fetchJobItems = async (
   searchText: string
 ): Promise<JobItemsApiResponse> => {
   const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
+
   const data = await response.json();
   return data;
 };
@@ -65,7 +71,7 @@ export function useJobItems(searchText: string) {
       retry: false,
       enabled: Boolean(searchText),
       onError: (error) => {
-        console.log(error);
+        toast.error(error.message);
       },
     }
   );
