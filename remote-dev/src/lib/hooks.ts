a7d323d0d81f8f1640e63f76db8ae5da77c92ea9
Ianthe23 <ivo.pastin@gmail.com>
2025-02-12 09:37:10 +0200

    changed fetch to react query

--- remote-dev/src/lib/hooks.ts
+++ remote-dev/src/lib/hooks.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { JobItem, JobItemExpanded } from "./types";
+import { JobItem } from "./types";
 import { BASE_API_URL } from "./constants";
+import { useQuery } from "@tanstack/react-query";
 
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -22,24 +23,44 @@ export function useActiveId() {
   return activeId;
 }
 
+// export function useJobItem(id: number | null) {
+//   const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+//   const [isLoading, setIsLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (!id) return;
+
+//     const fetchData = async () => {
+//       setIsLoading(true);
+//       const response = await fetch(`${BASE_API_URL}/${id}`);
+//       const data = await response.json();
+//       setIsLoading(false);
+//       setJobItem(data.jobItem);
+//     };
+
+//     fetchData();
+//   }, [id]);
+
+//   return { jobItem, isLoading } as const;
+// }
+
 export function useJobItem(id: number | null) {
-  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchData = async () => {
-      setIsLoading(true);
+  const { data, isLoading } = useQuery(
+    ["job-item", id],
+    async () => {
       const response = await fetch(`${BASE_API_URL}/${id}`);
       const data = await response.json();
-      setIsLoading(false);
-      setJobItem(data.jobItem);
-    };
-
-    fetchData();
-  }, [id]);
-
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 60, // 1 hour
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: () => {},
+    }
+  );
+  const jobItem = data.jobItem;
   return { jobItem, isLoading } as const;
 }
 
