f88a0672b7d635d73432220580d19de35c327022
Pastin Maria <ivo.pastin@gmail.com>
2025-03-07 18:41:24 +0200

    add loading skeletons and sleep utility for event pages

--- project-evento/src/app/events/[city]/page.tsx
+++ project-evento/src/app/events/[city]/page.tsx
@@ -1,6 +1,7 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
 import { EventoEvent } from "@/lib/types";
+import { sleep } from "@/lib/utils";
 
 type EventsTypeProps = {
   params: {
@@ -11,6 +12,7 @@ type EventsTypeProps = {
 export default async function EventsPage({ params }: EventsTypeProps) {
   const city = params.city;
 
+  await sleep(2000);
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
