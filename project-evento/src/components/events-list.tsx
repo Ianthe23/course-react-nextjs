09708f15bc7de514b04596658c4b1f027d1feefe
Pastin Maria <ivo.pastin@gmail.com>
2025-03-07 11:42:32 +0200

    replace tailwind-merge with custom cn utility for class name handling

--- project-evento/src/components/events-list.tsx
+++ project-evento/src/components/events-list.tsx
@@ -7,7 +7,7 @@ type EventsListProps = {
 
 export default function EventsList({ events }: EventsListProps) {
   return (
-    <section className=" max-w-[1100px] flex flex-wrap gap-10 justify-center px-20">
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
