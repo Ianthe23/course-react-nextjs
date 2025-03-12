68da0f603f6f59079c67f5c52eb42b8421b310da
Ianthe23 <ivo.pastin@gmail.com>
2025-03-12 17:32:06 +0200

    add metadata generation for event and events pages; implement capitalize utility function

--- project-evento/src/app/events/[city]/page.tsx
+++ project-evento/src/app/events/[city]/page.tsx
@@ -1,25 +1,35 @@
 import EventsList from "@/components/events-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
-import { sleep } from "@/lib/utils";
 import { Suspense } from "react";
 import Loading from "./loading";
+import { Metadata } from "next";
+import { capitalize } from "@/lib/utils";
 
-type EventsTypeProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-export default async function EventsPage({ params }: EventsTypeProps) {
+export function generateMetadata({ params }: Props): Metadata {
+  const city = params.city;
+
+  return {
+    title:
+      city === "all"
+        ? "All Events | Evento"
+        : `Events in ${capitalize(city)} | Evento`,
+  };
+}
+
+export default async function EventsPage({ params }: Props) {
   const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === "all" && "All events"}
-        {city !== "all" &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
 
       <Suspense fallback={<Loading />}>
